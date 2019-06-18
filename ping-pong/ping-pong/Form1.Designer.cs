cab16960cb976bb393973435737b82eb8ead34d9
Gaál Szilárd <gaalszilard23@gmail.com>
2019-06-18 15:11:00 +0200

    basic ping-pong game playable. meets most expectations

---
+++ ping-pong/ping-pong/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ping_pong
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameArena = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scoreBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.levelText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameArena)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // gameArena
+            // 
+            this.gameArena.BackColor = System.Drawing.Color.LawnGreen;
+            resources.ApplyResources(this.gameArena, "gameArena");
+            this.gameArena.Name = "gameArena";
+            this.gameArena.TabStop = false;
+            // 
+            // player
+            // 
+            resources.ApplyResources(this.player, "player");
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.player.Name = "player";
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            resources.ApplyResources(this.ball, "ball");
+            this.ball.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ball.Name = "ball";
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // scoreBox
+            // 
+            resources.ApplyResources(this.scoreBox, "scoreBox");
+            this.scoreBox.ForeColor = System.Drawing.Color.Black;
+            this.scoreBox.Name = "scoreBox";
+            this.scoreBox.ReadOnly = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // levelText
+            // 
+            this.levelText.BackColor = System.Drawing.Color.WhiteSmoke;
+            resources.ApplyResources(this.levelText, "levelText");
+            this.levelText.Name = "levelText";
+            this.levelText.ReadOnly = true;
+            // 
+            // mainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.levelText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scoreBox);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.gameArena);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            ((System.ComponentModel.ISupportInitialize)(this.gameArena)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox gameArena;
+        public System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox scoreBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox levelText;
+    }
+}
+
---
+++ ping-pong/ping-pong/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ping_pong
+{
+    public partial class mainForm : Form
+    { 
+        private readonly int leftWall = 20;
+        private readonly int rightWall = 312;
+        private readonly int topWall = 40;
+
+        private int directionX = -2;
+        private int directionY = 2;
+        private int speedMultiplyer = 1;
+
+        private int score = 0;
+        private int level = 1;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            FormClosing += mainForm_Close;
+            KeyDown += new KeyEventHandler(MainForm_KeyDown);
+            timer1.Interval = 10;
+            timer1.Start();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) this.Close();
+            else if (timer1.Enabled)
+            {
+                int x = player.Location.X;
+                int y = player.Location.Y;
+                if (e.KeyCode == Keys.Right && x <= gameArena.Width - 98) x += 5;
+                else if (e.KeyCode == Keys.Left && x >= 20) x -= 5;
+                else if (e.KeyCode == Keys.Space && timer1.Enabled) timer1.Stop();
+                player.Location = new Point(x, y);
+            }
+            else if (e.KeyCode == Keys.Space && !timer1.Enabled) timer1.Start();
+           
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)        
+        {
+            int x = ball.Location.X;
+            int y = ball.Location.Y;
+
+            if (y <= topWall)
+            {
+                directionY = -directionY;
+            }
+            else if (x <= leftWall || x >= rightWall)
+            {
+                directionX = -directionX;
+            }
+            else if (y >= player.Location.Y - 13 &&
+                    x <= player.Location.X + 113 &&
+                    x >= player.Location.X)
+            {
+                directionY = -directionY;
+                IncrementProgress();
+            }
+            else if (y >= 533)
+            {
+                gameOver();
+                return;
+            }
+            MoveBall(x, y);
+        }
+    
+        private void mainForm_Close(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you realy want to quit?\r\nYour score:" + score, "Exit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+            {
+                e.Cancel = true;
+            }   
+        }
+
+        private void gameOver()
+        {
+            timer1.Stop();
+            MessageBox.Show("Game over!\r\nYour score was:\r\n" + score);
+            speedMultiplyer = 1;
+            level = 1;
+            score = 0;
+            scoreBox.Text = score.ToString();
+            levelText.Text = level.ToString();
+            ball.Location = new Point(100, 50);
+          
+            timer1.Start();
+        }
+
+        private void MoveBall(int x, int y)
+        {
+            ball.Location = new Point(x + (directionX*speedMultiplyer), y + (directionY*speedMultiplyer));
+        }
+
+        private void IncrementProgress()
+        {
+            score += 1;
+            scoreBox.Text = score.ToString();
+            if (score % 5 == 0)
+            {
+                speedMultiplyer++;
+                level++;
+                levelText.Text = level.ToString();
+            }
+        }
+    }
+}
